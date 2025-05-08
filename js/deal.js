f162b84971b82eac0719d5f37cb4c298a7c435bf
Sazid <sazidtec06@gmail.com>
2025-05-08 10:30:43 +0600

    load pet by category name is processing

--- js/deal.js
+++ js/deal.js
@@ -1,35 +1,22 @@
 const loadCategory = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
     const data = await res.json();
-    setButton(data);
+    setButton(data.categories);
 }
 
 const setButton = (data) => {
     console.log(data)
     const categoryContainer = document.getElementById('categorySection');
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <div class="flex justify-between">
-            
-            <button class="btn px-10 py-7 font-bold">
-            <img class="w-1/3" src="${data.categories[0].category_icon}" />
-            ${data.categories[0].category}s</button>
-
-            <button class="btn px-10 py-7 font-bold">
-            <img class="w-1/3" src="${data.categories[1].category_icon}" />
-            ${data.categories[1].category}s</button>
-
-            <button class="btn px-10 py-7 font-bold">
-            <img class="w-1/3" src="${data.categories[2].category_icon}" />
-            ${data.categories[2].category}s</button>
-
-            <button class="btn px-10 py-7 font-bold">
-            <img class="w-1/3" src="${data.categories[3].category_icon}" />
-            ${data.categories[3].category}s</button>
-
-        </div>
-    `;
-    categoryContainer.appendChild(div);
+    for (const cat of data) {
+        const div = document.createElement('div');
+        console.log(cat.category);
+        div.innerHTML = `             
+            <button onclick="loadPetsByCategory(${cat.category})" class="btn py-6 px-7">
+            <img class="w-1/3" src="${cat.category_icon}"/>
+            ${cat.category}</button>     
+        `;
+        categoryContainer.appendChild(div);
+    }
 }
 
 const loadAllpets = async () => {
@@ -44,6 +31,7 @@ const loadAllpets = async () => {
 const displayAllpets = (data) => {
     // console.log(data)
     const petcontainer = document.getElementById('allpetscontainer');
+    petcontainer.innerHTML="";
     for (const pet of data) {
         // console.log(pet)
         const div = document.createElement('div')
@@ -57,27 +45,29 @@ const displayAllpets = (data) => {
         <div class="">
           <h2 class="card-title font-bold">${pet.pet_name}</h2>
             <div class="flex gap-1 text-gray-500">
-                <img width="20" height="20" src="https://img.icons8.com/sf-ultralight/25/list.png" alt="list"/>
+            <img class="w-[20px]" src="https://img.icons8.com/fluency-systems-regular/48/medium-icons.png" alt="medium-icons"/>
                 <p>Breed: ${pet.breed}</p>
             </div>
             <div class="flex gap-1 text-gray-500">
-            <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/calendar--v1.png" alt="calendar--v1"/>
+            <img class="w-[20px]" src="https://img.icons8.com/material-outlined/24/calendar--v1.png" alt="calendar--v1"/>
                 <p>Birth: ${pet.date_of_birth
-                }</p>
+            }</p>
             </div>
             <div class="flex gap-1 text-gray-500">
-                <img width="20" height="20" src="https://img.icons8.com/sf-ultralight/25/list.png" alt="list"/>
-                <p>Breed: ${pet.breed}</p>
+            <img class="w-[20px]" src="https://img.icons8.com/fluency-systems-regular/48/gender.png" alt="gender"/>
+                <p>Breed: ${pet.gender}</p>
             </div>
             <div class="flex gap-1 text-gray-500 pb-3">
-                <img width="20" height="20" src="https://img.icons8.com/sf-ultralight/25/list.png" alt="list"/>
-                <p>Breed: ${pet.breed}</p>
+            <img class="w-[20px]" src="https://img.icons8.com/material-outlined/24/us-dollar--v1.png" alt="us-dollar--v1"/>
+                <p>Breed: ${pet.price}$</p>
             </div>
             <hr>
-          <div class="card-actions justify-end pt-3">
-            <button class="btn text-blue-800"></button>
-            <button class="btn text-blue-800">Adopt</button>
-            <button class="btn text-blue-800">Details</button>
+          <div class="card-actions justify-between pt-3">
+            <button class="btn">
+                <img width="20" height="20" src="https://img.icons8.com/material-outlined/24/facebook-like--v1.png" alt="facebook-like--v1"/>
+            </button>
+            <button class="btn text-[#0E7A81]">Adopt</button>
+            <button class="btn text-[#0E7A81]">Details</button>
           </div>
         </div>
       </div>
@@ -86,6 +76,14 @@ const displayAllpets = (data) => {
     }
 }
 
+// fetch pets by category
+
+function loadPetsByCategory(categoryName){
+    alert(categoryName);
+    const res =  fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryName}`);
+    const data =  res.json();
+    console.log(data);
+}
 
 loadCategory();
 loadAllpets();
